b52cd95187427610349ccf344cad62cf33219624
Viktor Paliichuk <vpaliichuk@provectus.com>
2019-09-16 19:03:31 +0300

    Add all my files

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+            v-model="drawer"
+            app
+    >
+      <v-list dense>
+        <v-list-item to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+            app
+            color="indigo"
+            dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar>
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <div class="flex-grow-1"></div>
+
+        <v-toolbar-items>
+          <v-btn text to="/">Link 1</v-btn>
+          <v-btn text to="/about">Link 2</v-btn>
+          <v-btn text>Link 3</v-btn>
+        <!--  <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>-->
+        </v-toolbar-items>
+
+        <template v-if="$vuetify.breakpoint.smAndUp">
+          <v-btn icon>
+            <v-icon>mdi-export-variant</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-delete-circle</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-plus-circle</v-icon>
+          </v-btn>
+        </template>
+      </v-toolbar>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+              class="fill-height"
+              fluid
+      >
+        <router-view/>
+      </v-container>
+    </v-content>
+    <v-footer
+            color="indigo"
+            app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+    drawer: false
+  }),
+};
+</script>
